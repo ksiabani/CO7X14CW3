627ce341c823743301df25f00083360fee1aea9a
Konstantinos Siampanis <siampako@aris-lux.lan>
2018-03-16 17:28:37 +0200

    Refacor app

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-let OAuth = require('oauth').OAuth;
-let qs = require('qs');
+let Twitter = require('twitter-node-client').Twitter;
 
 let config = {
     "consumerKey": "Hf5ePvk7gctj7P0SSQuRCtKZb",
@@ -18,97 +17,22 @@ let success = function (data) {
 
 let twitter = new Twitter(config);
 
-//
-function Twitter(config) {
-    this.consumerKey = config.consumerKey;
-    this.consumerSecret = config.consumerSecret;
-    this.accessToken = config.accessToken;
-    this.accessTokenSecret = config.accessTokenSecret;
-    this.callBackUrl = config.callBackUrl;
-    this.baseUrl = 'https://api.twitter.com/1.1';
-    this.oauth = new OAuth(
-        'https://api.twitter.com/oauth/request_token',
-        'https://api.twitter.com/oauth/access_token',
-        this.consumerKey,
-        this.consumerSecret,
-        '1.0',
-        this.callBackUrl,
-        'HMAC-SHA1'
-    );
+
+// twitter.getUserTimeline({ screen_name: 'ksiabani', count: '10'}, error, success);
+// Twitter.prototype.getUserTimeline = function (params, error, success) {
+//     var path = '/statuses/user_timeline.json' + this.buildQS(params);
+//     var url = this.baseUrl + path;
+//     this.doRequest(url, error, success);
+// };
+
+
+// Get user timeline
+function getUserTimeline() {
+    let url = 'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=ksiabani&count=10';
+    twitter.doRequest(url, error, success);
 }
 
-Twitter.prototype.getOAuthRequestToken = function (next) {
-    this.oauth.getOAuthRequestToken(function (error, oauth_token, oauth_token_secret, results) {
-        if (error) {
-            console.log('ERROR: ' + error);
-            next();
-        }
-        else {
-            var oauth = {};
-            oauth.token = oauth_token;
-            oauth.token_secret = oauth_token_secret;
-            console.log('oauth.token: ' + oauth.token);
-            console.log('oauth.token_secret: ' + oauth.token_secret);
-            next(oauth);
-        }
-    });
-};
-
-Twitter.prototype.getOAuthAccessToken = function (oauth, next) {
-    this.oauth.getOAuthAccessToken(oauth.token, oauth.token_secret, oauth.verifier,
-        function (error, oauth_access_token, oauth_access_token_secret, results) {
-            if (error) {
-                console.log('ERROR: ' + error);
-                next();
-            } else {
-                oauth.access_token = oauth_access_token;
-                oauth.access_token_secret = oauth_access_token_secret;
-
-                console.log('oauth.token: ' + oauth.token);
-                console.log('oauth.token_secret: ' + oauth.token_secret);
-                console.log('oauth.access_token: ' + access_token.token);
-                console.log('oauth.access_token_secret: ' + oauth.access_token_secret);
-                next(oauth);
-            }
-        }
-    );
-};
-
-Twitter.prototype.buildQS = function (params) {
-    if (params && Object.keys(params).length > 0) {
-        return '?' + qs.stringify(params);
-    }
-    return '';
-};
-
-Twitter.prototype.doRequest = function (url, error, success) {
-    // Fix the mismatch between OAuth's  RFC3986's and Javascript's beliefs in what is right and wrong ;)
-    // From https://github.com/ttezel/twit/blob/master/lib/oarequest.js
-    url = url.replace(/\!/g, "%21")
-        .replace(/\'/g, "%27")
-        .replace(/\(/g, "%28")
-        .replace(/\)/g, "%29")
-        .replace(/\*/g, "%2A");
-
-    this.oauth.get(url, this.accessToken, this.accessTokenSecret, function (err, body, response) {
-        console.log('URL [%s]', url);
-        if (!err && response.statusCode == 200) {
-            success(body);
-        } else {
-            error(err, response, body);
-        }
-    });
-};
-
-Twitter.prototype.getUserTimeline = function (params, error, success) {
-    var path = '/statuses/user_timeline.json' + this.buildQS(params);
-    var url = this.baseUrl + path;
-    this.doRequest(url, error, success);
-};
-
-
-
-twitter.getUserTimeline({ screen_name: 'BoyCook', count: '10'}, error, success);
+getUserTimeline();
 
 
 
